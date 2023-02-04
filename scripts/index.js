286ef7381da197e3ff1f2b9b3f5172094626a33a
kk36 <lk0981@mail.ru>
2023-02-04 15:36:38 +0300

    fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,22 @@ const buttonEdit = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const buttonClose = document.querySelector('.popup__close');
 const form = document.querySelector('.popup__edit-form');
-let nameInput = document.querySelector('.popup__input_name');
+let nameInput = document.querySelector('.popup__input_field_name');
 let nameProfile = document.querySelector('.profile__name');
-let aboutInput = document.querySelector('.popup__input_about');
+let aboutInput = document.querySelector('.popup__input_field_about');
 let aboutProfile = document.querySelector('.profile__about');
 
 /* opens popup */
 const handleEditButtonClick = () => {
-  popup.classList.toggle('popup_opened');
+  popup.classList.add('popup_opened');
+
   nameInput.value =  nameProfile.textContent;
   aboutInput.value = aboutProfile.textContent;
 }
 
 /* closes popup */
 const handleCloseButtonClick = () => {
-  popup.classList.toggle('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 /* saves form info */
